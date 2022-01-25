6059201c8632063beeba0d432d7918b906fe9995
Lucas <lucasgunner3@gmail.com>
2022-01-24 22:44:43 -0300

    Criando secret do banco e gerando migrations

---
+++ src/CleanArchMvc.CrossCutting/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchMvc.Domain.Interfaces.Repositories;
+using CleanArchMvc.Infrastructure.Contexts;
+using CleanArchMvc.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.CrossCutting
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("SqlConnection");
+
+            services.AddDbContext<ApplicationDbContext>(
+                options =>
+                {
+                    options.UseSqlServer(connectionString, x =>
+                    {
+                        x.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
+                        x.EnableRetryOnFailure(3);
+                        x.CommandTimeout(10);
+                        x.MigrationsHistoryTable("Migrations");
+                    });
+                });
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
