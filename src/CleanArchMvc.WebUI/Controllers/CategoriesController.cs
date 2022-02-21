9064f9207bd8f196efcf62baffbffe5903a120a0
Lucas <lucasgunner3@gmail.com>
2022-02-20 22:38:33 -0300

    Products

--- src/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ src/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using CleanArchMvc.Application.DTOs.Categories;
+using CleanArchMvc.Application.DTOs.Products;
 using CleanArchMvc.Application.Interfaces.Services;
+using CleanArchMvc.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -19,6 +23,85 @@ namespace CleanArchMvc.WebUI.Controllers
             var categories = await _categoryService.GetCategories();
 
             return View(categories);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var category = await _categoryService.GetCategoryById(id);
+
+            if (category is null)
+            {
+                return View("Error", new ErrorViewModel()
+                {
+                    RequestId = Guid.NewGuid().ToString()
+                });
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var category = await _categoryService.GetCategoryById(id);
+
+            if (category is null)
+            {
+                return View("Error", new ErrorViewModel()
+                {
+                    RequestId = Guid.NewGuid().ToString()
+                });
+            }
+
+            ViewBag.OldCategory = category;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _categoryService.GetCategoryById(id);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Create(CreateCategoryDto category)
+        {
+            if (!ModelState.IsValid) return View(category);
+
+            await _categoryService.AddCategory(category);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Edit(UpdateCategoryDto category)
+        {
+            if (!ModelState.IsValid) return View(category);
+
+            await _categoryService.UpdateCategory(category);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.RemoveCategory(id);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- src/CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ src/CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,22 +1,85 @@
-﻿using System.Threading.Tasks;
+﻿using CleanArchMvc.Application.DTOs.Categories;
+using CleanArchMvc.Application.DTOs.Products;
 using CleanArchMvc.Application.Interfaces.Services;
+using CleanArchMvc.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService,
+                                  ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var products = await _productService.GetProducts();
             return View(products);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Categories = new SelectList(await GetCategoriesAsync(), "Id", "Name");
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await GetProductAsync(id);
+
+            if (product is null)
+            {
+                return View("Error", new ErrorViewModel()
+                {
+                    RequestId = Guid.NewGuid().ToString()
+                });
+            }
+
+            ViewBag.OldProduct = product;
+            ViewBag.Categories = new SelectList(await GetCategoriesAsync(), "Id", "Name", product.Category.Id);
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CreateProductDto product)
+        {
+            if (!ModelState.IsValid) return View(product);
+
+            await _productService.AddProduct(product);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditAsync(UpdateProductDto product)
+        {
+            if (!ModelState.IsValid) return View(product);
+
+            await _productService.UpdateProduct(product);
+
+            return RedirectToAction("Index");
+        }
+
+        private async Task<IEnumerable<CategoryDto>> GetCategoriesAsync() =>
+            await _categoryService.GetCategories();
+
+
+        private async Task<ProductDto> GetProductAsync(int productId) =>
+            await _productService.GetProductById(productId);
     }
 }
